f704a3e72f207f9e1132986d88e2bb70118a32a4
이승우 <swoolee97@ajou.ac.kr>
2024-10-23 17:23:35 +0900

    test

--- src/main/java/com/ssafyss/board_practice/BoardPracticeApplication.java
+++ src/main/java/com/ssafyss/board_practice/BoardPracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardPracticeApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(BoardPracticeApplication.class, args);
 	}
