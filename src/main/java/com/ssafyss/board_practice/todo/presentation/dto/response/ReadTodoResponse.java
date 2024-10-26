6d5d89813be507855d952c55b57799c62dbffe84
jhsseonn <hs125bt@gmail.com>
2024-10-27 01:39:04 +0900

    fix: response 객체를 json 형태로 직렬화 하지 못하는 문제 해결

--- src/main/java/com/ssafyss/board_practice/todo/presentation/dto/response/ReadTodoResponse.java
+++ src/main/java/com/ssafyss/board_practice/todo/presentation/dto/response/ReadTodoResponse.java
@@ -1,10 +1,17 @@
 package com.ssafyss.board_practice.todo.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafyss.board_practice.todo.application.dto.ReadTodoDto;
 
 public class ReadTodoResponse {
+
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("content")
     private String content;
+
+    @JsonProperty("completed")
     private boolean completed;
 
     public ReadTodoResponse(ReadTodoDto readTodoDto) {
