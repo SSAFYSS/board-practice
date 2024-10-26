29c3be60ee75816494f8450b8d6e230c2840640d
jhsseonn <hs125bt@gmail.com>
2024-10-27 01:46:55 +0900

    fix: json 데이터의 값을 deserialize 하지 못하는 문제 해결

--- src/main/java/com/ssafyss/board_practice/todo/presentation/dto/request/CreateTodoRequest.java
+++ src/main/java/com/ssafyss/board_practice/todo/presentation/dto/request/CreateTodoRequest.java
@@ -1,9 +1,17 @@
 package com.ssafyss.board_practice.todo.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CreateTodoRequest {
+
     private String content;
 
-    public CreateTodoRequest(String content) {
+    public CreateTodoRequest() {
+    }
+
+    @JsonCreator
+    public CreateTodoRequest(@JsonProperty String content) {
         this.content = content;
     }
 
