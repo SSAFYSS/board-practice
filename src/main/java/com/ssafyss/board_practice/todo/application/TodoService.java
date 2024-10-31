6f1018fbece17441b77e140d1f54706249d685d4
janghyoseon <hosu0125@naver.com>
2024-10-31 21:39:33 +0900

    refactor: jpa 도입에 따른 로직 변경

--- src/main/java/com/ssafyss/board_practice/todo/application/TodoService.java
+++ src/main/java/com/ssafyss/board_practice/todo/application/TodoService.java
@@ -6,6 +6,9 @@ import com.ssafyss.board_practice.todo.application.dto.ReadTodoDto;
 import com.ssafyss.board_practice.todo.domain.Todo;
 import com.ssafyss.board_practice.todo.infrastructure.repository.TodoRepository;
 import java.util.List;
+
+import com.ssafyss.board_practice.user.domain.User;
+import com.ssafyss.board_practice.user.infrastructure.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,16 +20,20 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Transactional(readOnly = true)
     public List<ReadTodoDto> readAllTodos() {
-        final List<Todo> readTodos = todoRepository.findAllTodo();
+        final List<Todo> readTodos = todoRepository.findAllByDeletedFalse();
         return readTodos.stream().map(ReadTodoDto::new).toList();
     }
 
     public ReadTodoDetailDto createTodo(CreateTodoDto createTodoDto) {
-        Todo todo = new Todo(createTodoDto.getUserId(), createTodoDto.getContent());
-        todoRepository.createTodo(todo);
-        final Todo createdTodo = todoRepository.findById(todo.getId());
+        User user = userRepository.findById(createTodoDto.getUserId()).orElseThrow();
+        Todo todo = new Todo(user, createTodoDto.getContent());
+        todoRepository.save(todo);
+        final Todo createdTodo = todoRepository.findById(todo.getId()).orElseThrow();
         return new ReadTodoDetailDto(createdTodo);
     }
 }
